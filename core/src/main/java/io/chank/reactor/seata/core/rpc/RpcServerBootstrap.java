a9d24f8918776ca8637da13d8ebc4bf2e093e834
chank <fangliquan@qq.com>
2020-07-19 16:22:40 +0800

    Init core

---
+++ core/src/main/java/io/chank/reactor/seata/core/rpc/RpcServerBootstrap.java
@@ -0,0 +1,14 @@
+package io.chank.reactor.seata.core.rpc;
+
+import io.seata.core.rpc.RemotingBootstrap;
+
+public class RpcServerBootstrap implements RemotingBootstrap {
+
+    public void start() {
+
+    }
+
+    public void shutdown() {
+
+    }
+}
